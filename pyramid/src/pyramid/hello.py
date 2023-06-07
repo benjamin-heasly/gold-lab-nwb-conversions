2c8f54709431087deba4f88a30440c7107d41040
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-06-07 12:59:26 -0400

    Test: can we execute matplot lib commands during CI tests?

--- pyramid/src/pyramid/hello.py
+++ pyramid/src/pyramid/hello.py
@@ -27,6 +27,8 @@ def plot(timeout: float = 10):
     So here's a simplified example where I'll try things out.
 
     I'm running this manually from a local terminal, in this "pyramid" dir, in the gold_nwb conda environment.
+    $ conda activate gold_nwb
+    $ cd pyramid
     $ pip install .
     $ python
     >>> from pyramid import hello
@@ -36,11 +38,15 @@ def plot(timeout: float = 10):
     # use matplotlib in interactive mode instead of blocking on plt.show().
     plt.ion()
 
-    fig, ax = plt.subplots()
-    th = np.linspace(0, 2*np.pi, 512)
-    ax.set_ylim(-1.5, 1.5)
+    fig1, ax1 = plt.subplots()
+    th1 = np.linspace(0, 2*np.pi, 512)
+    ax1.set_ylim(-1.5, 1.5)
+    ln1, = ax1.plot(th1, np.sin(th1))
 
-    ln, = ax.plot(th, np.sin(th))
+    fig2, ax2 = plt.subplots()
+    th2 = np.linspace(0, 2*np.pi, 512)
+    ax2.set_ylim(-1.5, 1.5)
+    ln2, = ax2.plot(th2, np.cos(th2))
 
     start_time = time.time()
     uptime = time.time() - start_time
@@ -48,13 +54,36 @@ def plot(timeout: float = 10):
         # check for trial data
         time.sleep(.01)
 
-        # redraw the plot
-        ln.set_ydata(np.sin(th + uptime))
-        ln.figure.canvas.draw_idle()
+        # update and redraw each plot
+        if plt.fignum_exists(fig1.number):
+            ln1.set_ydata(np.sin(th1 + uptime))
+            fig1.canvas.draw_idle()
+            fig1.canvas.flush_events()
 
-        # respond to plot window events, including user inputs
-        ln.figure.canvas.flush_events()
+        # update and redraw each plot
+        if plt.fignum_exists(fig2.number):
+            ln2.set_ydata(np.cos(th2 + uptime))
+            fig2.canvas.draw_idle()
+            fig2.canvas.flush_events()
 
         uptime = time.time() - start_time
 
-    plt.close(fig)
+    if plt.fignum_exists(fig1.number):
+        plt.close(fig1)
+
+    if plt.fignum_exists(fig2.number):
+        plt.close(fig2)
+
+# What's a "plot" going to do?
+# Init a managed figure
+# Init with experiment and subject info
+# Init user axes / subplots and stash references
+# Init user lines etc. and stash references
+# Init user other state and stash?
+# Check if managed figures still exists
+# Update with experiment info, subject info, current trial, trial extraction info
+# Update user lines etc.
+# Update user other state?
+# Redraw managed figure canvas
+# Flush managed figure canvas events
+# Close managed figure
--- pyramid/tests/pyramid/test_hello.py
+++ pyramid/tests/pyramid/test_hello.py
@@ -1,5 +1,5 @@
 from pyramid.__about__ import __version__
-from pyramid.hello import hello
+from pyramid.hello import hello, plot
 
 def test_hello():
     word = hello()
@@ -7,3 +7,6 @@ def test_hello():
 
 def test_version():
     assert __version__ == "0.0.1"
+
+def test_plot():
+    plot(1)
