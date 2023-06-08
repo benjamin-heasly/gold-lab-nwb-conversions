d75aeb17b0e9efcddc6c37e2089963d20b520487
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-06-08 11:59:51 -0400

    Tests for gui figure controller.

--- pyramid/src/pyramid/gui.py
+++ pyramid/src/pyramid/gui.py
@@ -11,15 +11,15 @@ class Plotter():
 
     def set_up(self, fig: Figure, experiment_info={}, subject_info={}) -> None:
         """Use the given fig to set up and store any axes, lines, user data, etc for this plot."""
-        pass
+        pass # pragma: no cover
 
     def update(self, fig: Figure, current_trial, trials_info, experiment_info={}, subject_info={}) -> None:
         """Update stored axes, lines, user data, etc for the current trial."""
-        pass
+        pass # pragma: no cover
 
     def clean_up(self, fig: Figure) -> None:
         """Clean up when it's time to go, if needed."""
-        pass
+        pass # pragma: no cover
 
     @classmethod
     def from_dynamic_import(cls, import_spec: str, external_package_path: str = None) -> Self:
@@ -90,7 +90,7 @@ class PlotFigureController():
 
         # Let each plotter set itself up.
         for plotter, fig in self.figures.items():
-            plotter.setup(fig, self.experiment_info, self.subject_info)
+            plotter.set_up(fig, self.experiment_info, self.subject_info)
 
     def update(self, current_trial, trials_info) -> None:
         # Let each plotter update for the current trial.
@@ -103,6 +103,6 @@ class PlotFigureController():
     def clean_up(self) -> None:
         # Close each managed figure.
         for plotter, fig in self.figures.items():
+            plotter.clean_up(fig)
             if plt.fignum_exists(fig.number):
-                plotter.clean_up(fig)
                 plt.close(fig)
---
+++ pyramid/tests/pyramid/test_gui.py
@@ -0,0 +1,104 @@
+import sys
+from pathlib import Path
+from pytest import fixture
+import matplotlib.pyplot as plt
+
+
+from pyramid.gui import Plotter, PlotFigureController
+from pyramid.plotters.sample_plotters import SampleSinePlotter, SampleCosinePlotter
+
+@fixture
+def tests_path(request):
+    this_file = Path(request.module.__file__)
+    return this_file.parent
+
+
+def test_installed_plotter_dynamic_import():
+    #Import a plotter that was installed in the usual way (eg pip) along with pyramid itself.
+    import_spec = "pyramid.plotters.sample_plotters.SampleSinePlotter"
+    plotter = Plotter.from_dynamic_import(import_spec)
+    assert isinstance(plotter, Plotter)
+    assert isinstance(plotter, SampleSinePlotter)
+
+
+def test_another_installed_plotter_dynamic_import():
+    import_spec = "pyramid.plotters.sample_plotters.SampleCosinePlotter"
+    plotter = Plotter.from_dynamic_import(import_spec)
+    assert isinstance(plotter, Plotter)
+    assert isinstance(plotter, SampleCosinePlotter)
+
+
+def test_external_plotter_dynamic_import(tests_path):
+    # Import a plotter from a local file that was not installed in a standard location (eg by pip).
+    # We don't want to litter the sys.path, so check we cleaned up after importing.
+    original_sys_path = sys.path.copy()
+    plotter = Plotter.from_dynamic_import('external_package.plotter_module.ExternalPlotter1', tests_path.as_posix())
+    assert isinstance(plotter, Plotter)
+    assert sys.path == original_sys_path
+
+
+def test_another_external_plotter_dynamic_import(tests_path):
+    original_sys_path = sys.path.copy()
+    plotter = Plotter.from_dynamic_import('external_package.plotter_module.ExternalPlotter2', tests_path.as_posix())
+    assert isinstance(plotter, Plotter)
+    assert sys.path == original_sys_path
+
+
+def test_single_figure():
+    plotter = SampleSinePlotter()
+    controller = PlotFigureController(plotters = [plotter])
+    controller.set_up()
+    assert plotter.update_count == 0
+
+    controller.update(None, None)
+    assert plotter.update_count == 1
+
+    controller.update(None, None)
+    assert plotter.update_count == 2
+
+    controller.clean_up()
+    assert plotter.update_count == -1
+
+
+def test_multiple_figures():
+    plotters = [SampleSinePlotter(), SampleCosinePlotter(), SampleSinePlotter()]
+    controller = PlotFigureController(plotters = plotters)
+    controller.set_up()
+    for plotter in plotters:
+        assert plotter.update_count == 0
+
+    controller.update(None, None)
+    for plotter in plotters:
+        assert plotter.update_count == 1
+
+    controller.update(None, None)
+    for plotter in plotters:
+        assert plotter.update_count == 2
+
+    controller.clean_up()
+    for plotter in plotters:
+        assert plotter.update_count == -1
+
+
+def test_close_figure_early():
+    plotters = [SampleSinePlotter(), SampleCosinePlotter(), SampleSinePlotter()]
+    controller = PlotFigureController(plotters = plotters)
+    controller.set_up()
+    for plotter in plotters:
+        assert plotter.update_count == 0
+
+    controller.update(None, None)
+    for plotter in plotters:
+        assert plotter.update_count == 1
+
+    victim_figure = controller.figures[plotters[1]]
+    plt.close(victim_figure)
+
+    controller.update(None, None)
+    assert plotters[0].update_count == 2
+    assert plotters[1].update_count == 1
+    assert plotters[2].update_count == 2
+
+    controller.clean_up()
+    for plotter in plotters:
+        assert plotter.update_count == -1
