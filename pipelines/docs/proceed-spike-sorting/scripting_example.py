107e955b9c5ae9145074ce7b2714d14552d31953
Ben Heasly <benjamin.heasly@gmail.com>
2023-06-09 15:21:54 -0400

    Fix some silly example comment typos.

--- pipelines/docs/proceed-spike-sorting/scripting_example.py
+++ pipelines/docs/proceed-spike-sorting/scripting_example.py
@@ -1,14 +1,14 @@
 from proceed.cli import main
 
-# We got these Python variabls from somehere.
+# We got these Python variables from somehere.
 # We'd like to pass them in to a Proceed pipeline run.
 first_thing = "cool"
 second_thing = "script"
 
-# Instead of one command line on the shell, with parts separated by spaces,
-# we pass the command as a Python list of string elements.
-# Some of the command parts have internal structure, like "name=value" assignments.
-# Python "f-strings" are a handy way to build these, like f"name={value}".
+# On the shell we pass one long command string with parts separated by spaces.
+# Here, a list of strings, where each list element corresponds to a command part.
+# Some of Proceed's command parts have internal structure, like "name=value" args assignments.
+# We can build each of these using Python "f-string" formatting, like f"name={value}".
 command = [
     "run",
     "scripting-example.yaml",
