a383b260b03753114f49768535f10187db1c1032
Ben Heasly <benjamin.heasly@gmail.com>
2023-06-09 12:26:21 -0400

    Fix typo, sin->cos for sample cosine plotter.

--- pyramid/src/pyramid/plotters/sample_plotters.py
+++ pyramid/src/pyramid/plotters/sample_plotters.py
@@ -30,13 +30,13 @@ class SampleCosinePlotter(Plotter):
         ax = fig.subplots()
         ax.set_ylim(-1.25, 1.25)
         self.x_values = np.linspace(0, 2*np.pi, 512)
-        self.line, = ax.plot(self.x_values, np.sin(self.x_values))
+        self.line, = ax.plot(self.x_values, np.cos(self.x_values))
         self.start_time = time.time()
         self.update_count = 0
 
     def update(self, fig, current_trial, trials_info, experiment_info={}, subject_info={}) -> None:
         elapsed = time.time() - self.start_time
-        self.line.set_ydata(np.sin(self.x_values + elapsed))
+        self.line.set_ydata(np.cos(self.x_values + elapsed))
         self.update_count += 1
 
     def clean_up(self, fig: Figure) -> None:
