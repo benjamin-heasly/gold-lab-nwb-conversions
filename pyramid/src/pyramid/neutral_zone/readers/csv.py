22c36dda0b8fb1015837bf33ea9100be2a2d943d
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-07-05 18:45:33 -0400

    Begin implementing and testing CSV signal reader.

--- pyramid/src/pyramid/neutral_zone/readers/csv.py
+++ pyramid/src/pyramid/neutral_zone/readers/csv.py
@@ -3,8 +3,9 @@ import logging
 import csv
 import numpy as np
 
-from pyramid.model.events import NumericEventList
 from pyramid.model.model import BufferData
+from pyramid.model.events import NumericEventList
+from pyramid.model.signals import SignalChunk
 from pyramid.neutral_zone.readers.readers import Reader
 
 
@@ -53,7 +54,7 @@ class CsvNumericEventReader(Reader):
             self.file_stream = None
         self.csv_reader = None
 
-    def read_next(self) -> dict[str, NumericEventList]:
+    def read_next(self) -> dict[str, BufferData]:
         line_num = self.csv_reader.line_num
         next_row = self.csv_reader.__next__()
         try:
@@ -74,8 +75,113 @@ class CsvNumericEventReader(Reader):
             column_count = len(first_row)
         except Exception:
             column_count = 2
-            logging.error(f"Unable to read column count from CSV file {self.csv_file}, using default {column_count}", exc_info=True)
+            logging.error(
+                f"Unable to read column count from CSV file {self.csv_file}, using default {column_count}",
+                exc_info=True)
 
         return {
             self.results_key: NumericEventList(np.empty([0, column_count]))
         }
+
+
+class CsvSignalReader(Reader):
+    """Read numeric signals from a CSV of numbers.
+
+    Expects a header line with channel ids.
+    Skips any other lines that contain non-numeric values.
+    """
+
+    def __init__(
+        self,
+        csv_file: str = None,
+        sample_frequency: float = 1.0,
+        first_sample_time: float = 0.0,
+        lines_per_chunk: int = 10,
+        results_key: str = "samples",
+        dialect: str = 'excel',
+        **fmtparams
+    ) -> None:
+        self.csv_file = csv_file
+        self.sample_frequency = sample_frequency
+        self.next_sample_time = first_sample_time
+        self.lines_per_chunk = lines_per_chunk
+        self.results_key = results_key
+        self.dialect = dialect
+        self.fmtparams = fmtparams
+
+        self.file_stream = None
+        self.csv_reader = None
+        self.channel_ids = None
+
+    def __eq__(self, other: object) -> bool:
+        """Compare CSV readers field-wise, to support use of this class in tests."""
+        if isinstance(other, self.__class__):
+            return (
+                self.csv_file == other.csv_file
+                and self.sample_frequency == other.sample_frequency
+                and self.next_sample_time == other.next_sample_time
+                and self.lines_per_chunk == other.lines_per_chunk
+                and self.results_key == other.results_key
+                and self.dialect == other.dialect
+                and self.fmtparams == other.fmtparams
+            )
+        else:  # pragma: no cover
+            return False
+
+    def __enter__(self) -> Self:
+        # See https://docs.python.org/3/library/csv.html#id3 for why this has newline=''
+        self.file_stream = open(self.csv_file, mode='r', newline='')
+        self.csv_reader = csv.reader(self.file_stream, self.dialect, **self.fmtparams)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        if self.file_stream:
+            self.file_stream.close()
+            self.file_stream = None
+        self.csv_reader = None
+
+    def read_next(self) -> dict[str, BufferData]:
+        chunk = []
+        while len(chunk) < self.lines_per_chunk:
+            line_num = self.csv_reader.line_num 
+            try:
+                next_row = self.csv_reader.__next__()
+            except StopIteration:
+                # We reached the end.  We still want to return the last, partial chunk below.
+                break
+
+            try:
+                numeric_row = [float(element) for element in next_row]
+                chunk.append(numeric_row)
+            except ValueError as error:
+                logging.info(f"Skipping CSV '{self.csv_file}' line {line_num} <{next_row}> because {error.args}")
+                continue
+
+        if chunk:
+            # We got a complete chunk, or the last, partial chunk.
+            signal_chunk = SignalChunk(
+                np.array(chunk),
+                self.sample_frequency,
+                self.next_sample_time,
+                self.channel_ids
+            )
+            self.next_sample_time += len(chunk) / self.sample_frequency
+            return {self.results_key: signal_chunk}
+        else:
+            # We're really at the end, past the last chunk, signal stop to the caller.
+            raise StopIteration
+
+    def get_initial(self) -> dict[str, BufferData]:
+        # Consume the header row to get channel ids.
+        try:
+            self.channel_ids = self.csv_reader.__next__()
+        except StopIteration:
+            self.channel_ids = []
+
+        initial = SignalChunk(
+            np.empty([0, len(self.channel_ids)]),
+            self.sample_frequency,
+            self.next_sample_time,
+            self.channel_ids
+        )
+        return {self.results_key: initial}
--- pyramid/tests/pyramid/neutral_zone/readers/test_csv.py
+++ pyramid/tests/pyramid/neutral_zone/readers/test_csv.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from pytest import fixture, raises
 
 from pyramid.model.events import NumericEventList
-from pyramid.neutral_zone.readers.csv import CsvNumericEventReader
+from pyramid.model.signals import SignalChunk
+from pyramid.neutral_zone.readers.csv import CsvNumericEventReader, CsvSignalReader
 
 
 @fixture
@@ -13,8 +14,8 @@ def fixture_path(request):
     return Path(this_file.parent, 'fixture_files')
 
 
-def test_safe_to_spam_exit(fixture_path):
-    csv_file = Path(fixture_path, 'empty.csv').as_posix()
+def test_numeric_events_safe_to_spam_exit(fixture_path):
+    csv_file = Path(fixture_path, 'numeric_events', 'empty.csv').as_posix()
     reader = CsvNumericEventReader(csv_file)
     reader.__exit__(None, None, None)
     reader.__enter__()
@@ -24,19 +25,30 @@ def test_safe_to_spam_exit(fixture_path):
     assert reader.file_stream is None
 
 
-def test_empty_file(fixture_path):
-    csv_file = Path(fixture_path, 'empty.csv').as_posix()
+def test_numeric_events_empty_file(fixture_path):
+    csv_file = Path(fixture_path, 'numeric_events', 'empty.csv').as_posix()
     with CsvNumericEventReader(csv_file) as reader:
+        initial = reader.get_initial()
         with raises(StopIteration) as exception_info:
             reader.read_next()
-        assert exception_info.errisinstance(StopIteration)
 
+    expected_initial = {
+        reader.results_key: NumericEventList(np.empty([0, 2]))
+    }
+    assert initial == expected_initial
+    assert exception_info.errisinstance(StopIteration)
     assert reader.file_stream is None
 
 
-def test_csv_with_header_line(fixture_path):
-    csv_file = Path(fixture_path, 'header_line.csv').as_posix()
+def test_numeric_events_with_header_line(fixture_path):
+    csv_file = Path(fixture_path, 'numeric_events', 'header_line.csv').as_posix()
     with CsvNumericEventReader(csv_file) as reader:
+        initial = reader.get_initial()
+        expected_initial = {
+            reader.results_key: NumericEventList(np.empty([0, 3]))
+        }
+        assert initial == expected_initial
+
         # Consume the header line.
         assert reader.read_next() is None
 
@@ -55,9 +67,15 @@ def test_csv_with_header_line(fixture_path):
     assert reader.file_stream is None
 
 
-def test_csv_with_no_header_line(fixture_path):
-    csv_file = Path(fixture_path, 'no_header_line.csv').as_posix()
+def test_numeric_events_with_no_header_line(fixture_path):
+    csv_file = Path(fixture_path, 'numeric_events', 'no_header_line.csv').as_posix()
     with CsvNumericEventReader(csv_file) as reader:
+        initial = reader.get_initial()
+        expected_initial = {
+            reader.results_key: NumericEventList(np.empty([0, 3]))
+        }
+        assert initial == expected_initial
+
         # Read 32 lines...
         for t in range(32):
             result = reader.read_next()
@@ -73,10 +91,16 @@ def test_csv_with_no_header_line(fixture_path):
     assert reader.file_stream is None
 
 
-def test_csv_skip_nonnumeric_lines(fixture_path):
-    csv_file = Path(fixture_path, 'nonnumeric_lines.csv').as_posix()
+def test_numeric_events_skip_nonnumeric_lines(fixture_path):
+    csv_file = Path(fixture_path, 'numeric_events', 'nonnumeric_lines.csv').as_posix()
     nonnumeric_lines = [1, 11, 15, 21, 28]
     with CsvNumericEventReader(csv_file) as reader:
+        initial = reader.get_initial()
+        expected_initial = {
+            reader.results_key: NumericEventList(np.empty([0, 3]))
+        }
+        assert initial == expected_initial
+
         # Read 32 lines...
         for t in range(32):
             result = reader.read_next()
@@ -93,3 +117,99 @@ def test_csv_skip_nonnumeric_lines(fixture_path):
         assert exception_info.errisinstance(StopIteration)
 
     assert reader.file_stream is None
+
+
+def test_signals_safe_to_spam_exit(fixture_path):
+    csv_file = Path(fixture_path, 'signals', 'empty.csv').as_posix()
+    reader = CsvSignalReader(csv_file)
+    reader.__exit__(None, None, None)
+    reader.__enter__()
+    reader.__exit__(None, None, None)
+    reader.__exit__(None, None, None)
+
+    assert reader.file_stream is None
+
+
+def test_signals_empty_file(fixture_path):
+    csv_file = Path(fixture_path, 'signals', 'empty.csv').as_posix()
+    with CsvSignalReader(csv_file) as reader:
+        initial = reader.get_initial()
+        with raises(StopIteration) as exception_info:
+            reader.read_next()
+
+    expected_initial = {reader.results_key: SignalChunk(
+        np.empty([0, 0]), reader.sample_frequency, first_sample_time=0.0, channel_ids=[])}
+    assert initial == expected_initial
+    assert exception_info.errisinstance(StopIteration)
+    assert reader.file_stream is None
+
+
+def test_signals_complete_chunks(fixture_path):
+    csv_file = Path(fixture_path, 'signals', 'header_line.csv').as_posix()
+    with CsvSignalReader(csv_file, lines_per_chunk=10) as reader:
+        initial = reader.get_initial()
+        expected_initial = {
+            reader.results_key: SignalChunk(
+                np.empty([0, 3]),
+                reader.sample_frequency,
+                first_sample_time=0.0,
+                channel_ids=["a", "b", "c"]
+            )
+        }
+        assert initial == expected_initial
+
+        # Read 15 chunks of 10 lines each...
+        for t in range(15):
+            result = reader.read_next()
+            signal_chunk = result[reader.results_key]
+            assert signal_chunk.sample_count() == 10
+            # TODO: check the samples, too
+
+        # ...then be done.
+        with raises(StopIteration) as exception_info:
+            reader.read_next()
+        assert exception_info.errisinstance(StopIteration)
+
+    assert reader.file_stream is None
+
+
+def test_signals_last_partial_chunk(fixture_path):
+    csv_file = Path(fixture_path, 'signals', 'header_line.csv').as_posix()
+    with CsvSignalReader(csv_file, lines_per_chunk=11) as reader:
+        initial = reader.get_initial()
+        expected_initial = {
+            reader.results_key: SignalChunk(
+                np.empty([0, 3]),
+                reader.sample_frequency,
+                first_sample_time=0.0,
+                channel_ids=["a", "b", "c"]
+            )
+        }
+        assert initial == expected_initial
+
+        # Read 13 chunks of 11 lines each...
+        for t in range(13):
+            result = reader.read_next()
+            signal_chunk = result[reader.results_key]
+            assert signal_chunk.sample_count() == 11
+            # TODO: check the samples, too
+
+        # Read a last, partial chunk of 7 lines.
+        result = reader.read_next()
+        signal_chunk = result[reader.results_key]
+        assert signal_chunk.sample_count() == 7
+        # TODO: check the samples, too
+
+        # ...then be done.
+        with raises(StopIteration) as exception_info:
+            reader.read_next()
+        assert exception_info.errisinstance(StopIteration)
+
+    assert reader.file_stream is None
+
+
+# TODO: make signal fixture csv with nonnumeric lines 
+# TODO: signals skips nonnumeric lines
+
+# TODO: test csv signal reader equality, or remove the eq implementation
+# TODO: an end-to-end (somewhere else) test that includes signal data
