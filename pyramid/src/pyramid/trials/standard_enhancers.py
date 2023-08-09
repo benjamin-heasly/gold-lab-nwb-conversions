32df83725dbe7a1d3e0e6469534a1af573aa2001
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-08-09 14:00:04 -0400

    Implement Pyramid EventTimesEnhancer.

--- pyramid/src/pyramid/trials/standard_enhancers.py
+++ pyramid/src/pyramid/trials/standard_enhancers.py
@@ -30,22 +30,26 @@ class PairedCodesEnhancer(TrialEnhancer):
 
     buffer_name is the name of a buffer of NumericEventList.
 
-    rules_csv is a .csv file where each row represents a property to extract from the named buffer
-    and the rules for how to extract each property.  The .csv must have the following columns:
+    rules_csv is a .csv file where each row contains a rule for how to extract a property from the
+    named buffer.  The .csv must have the following columns:
+
         - "type": Used to select relevant rows of the .csv, and also the trial enhancement category to
                   use for each property.  By defalt only types "id" and "value" will be used.
                   Pass in rule_types to change this default.
         - "value": a numeric value that represents a property, for example 1010
         - "name": the string name to use for the property, for example "fp_on"
-        - "base": the base value to subtract from the property's value events, for example 7000
         - "min": the smallest event value to consier when looking for the property's value events
         - "max": the largest event value to consier when looking for the property's value events
-    The .csv may contain additional columns, which will be ignored (eg "comment").
+        - "base": the base value to subtract from the property's value events, for example 7000
+        - "scale": how to scale each event value after subtracting its base, for example 0.1
+
+    The .csv may contain additional columns, which will be ignored (eg a "comment" column).
 
     value_index is which event value to look for, in the NumericEventList
     (default is 0, the first value for each event).
 
     rule_types is a list of strings to match against the .csv "type" column.
+    The default is ["id", "value"].
 
     dialect and any additional fmtparams are passed on to the .csv reader.
     """
@@ -102,3 +106,70 @@ class PairedCodesEnhancer(TrialEnhancer):
                     values = value_list.get_values(start_time=property_time, value_index=self.value_index)
                     if values.size > 0:
                         trial.add_enhancement(rule['name'], values[0], rule['type'])
+
+
+class EventTimesEnhancer(TrialEnhancer):
+    """Look for times when named events occurred.
+
+    buffer_name is the name of a buffer of NumericEventList.
+
+    rules_csv is a .csv file where each row contains a rule for how to extract events from the
+    named buffer.  The .csv must have the following columns:
+
+        - "type": Used to select relevant rows of the .csv, and also the trial enhancement category to
+                  use for each property.  By defalt only the type "time" will be used.
+                  Pass in rule_types to change this default.
+        - "value": a numeric value that represents a property, for example 1010
+        - "name": the string name to use for the property, for example "fp_on"
+
+    The .csv may contain additional columns, which will be ignored (eg a "comment" column).
+
+    value_index is which event value to look for, in the NumericEventList
+    (default is 0, the first value for each event).
+
+    rule_types is a list of strings to match against the .csv "type" column.
+    The default is ["time"].
+
+    dialect and any additional fmtparams are passed on to the .csv reader.
+    """
+
+    def __init__(
+        self,
+        buffer_name: str,
+        rules_csv: str,
+        value_index: int = 0,
+        rule_types: list[str] = ["time"],
+        dialect: str = 'excel',
+        **fmtparams
+    ) -> None:
+        self.buffer_name = buffer_name
+        self.rules_csv = rules_csv
+        self.value_index = value_index
+        self.rule_types = rule_types
+        self.dialect = dialect
+        self.fmtparams = fmtparams
+
+        rules = {}
+        with open(self.rules_csv, mode='r', newline='') as f:
+            csv_reader = csv.DictReader(f, dialect=self.dialect, **self.fmtparams)
+            for row in csv_reader:
+                if row['type'] in self.rule_types:
+                    value = float(row['value'])
+                    rules[value] = {
+                        'type': row['type'],
+                        'name': row['name'],
+                    }
+        self.rules = rules
+
+    def enhance(
+        self,
+        trial: Trial,
+        trial_count: int,
+        experiment_info: dict[str: Any],
+        subject_info: dict[str: Any]
+    ) -> None:
+        event_list = trial.numeric_events[self.buffer_name]
+        for value, rule in self.rules.items():
+            # Did / when did this trial contain events of interest with the requested value?
+            event_times = event_list.get_times_of(value, self.value_index)
+            trial.add_enhancement(rule['name'], event_times.tolist(), rule['type'])
--- pyramid/tests/pyramid/trials/test_standard_enhancers.py
+++ pyramid/tests/pyramid/trials/test_standard_enhancers.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from pyramid.model.events import NumericEventList
 from pyramid.trials.trials import Trial
-from pyramid.trials.standard_enhancers import PairedCodesEnhancer
+from pyramid.trials.standard_enhancers import PairedCodesEnhancer, EventTimesEnhancer
 
 
 def test_paired_codes_enhancer(tmp_path):
@@ -22,13 +22,13 @@ def test_paired_codes_enhancer(tmp_path):
         rules_csv=rules_csv
     )
 
-    # The "id" and "value" types should be included.
+    # The "id" and "value" rows should be included.
     assert 42 in enhancer.rules.keys()
     assert 43 in enhancer.rules.keys()
     assert 44 in enhancer.rules.keys()
     assert 45 in enhancer.rules.keys()
 
-    # Other types should ne ignored.
+    # Other rows should ne ignored.
     assert 777 not in enhancer.rules.keys()
 
     paired_code_data = [
@@ -69,6 +69,59 @@ def test_paired_codes_enhancer(tmp_path):
         "value": {
             "baz": 151.0,
             "quux": 2.5250000000000004,
+        }
+    }
+    assert trial.enhancements == expected_enhancements
+
+
+def test_event_times_enhancer(tmp_path):
+    # Write out a .csv file with rules in it.
+    rules_csv = Path(tmp_path, "rules.csv")
+    with open(rules_csv, 'w') as f:
+        f.write('type,value,name,comment\n')
+        f.write('time,42,foo,this is just a comment\n')
+        f.write('time,43,bar,this is just a comment\n')
+        f.write('time,44,baz,this is just a comment\n')
+        f.write('ignore,777,this is just a comment\n')
+
+    enhancer = EventTimesEnhancer(
+        buffer_name="events",
+        rules_csv=rules_csv
+    )
+
+    # The "time" rows should be included.
+    assert 42 in enhancer.rules.keys()
+    assert 43 in enhancer.rules.keys()
+    assert 44 in enhancer.rules.keys()
+
+    # Other rows should ne ignored.
+    assert 777 not in enhancer.rules.keys()
+
+    event_data = [
+        [0.0, 42.0],    # code for event "foo"
+        [1, 3000],      # irrelevant
+        [2, 43],        # code for event "bar"
+        [3, 3005],      # irrelevant
+        [4, 13],        # irrelevant
+        [5, 42.0],      # code for event "foo" (again)
+        [6, 10000],     # irrelevant
+    ]
+    event_list = NumericEventList(event_data=np.array(event_data))
+    trial = Trial(
+        start_time=0,
+        end_time=20,
+        wrt_time=0,
+        numeric_events={
+            "events": event_list
+        }
+    )
+
+    enhancer.enhance(trial, 0, {}, {})
+    expected_enhancements = {
+        "time": {
+            "foo": [0.0, 5.0],
+            "bar": [2.0],
+            "baz": []
         }
     }
     assert trial.enhancements == expected_enhancements
