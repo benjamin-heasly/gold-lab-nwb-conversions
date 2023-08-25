5d244e4535236ed36932b30f0a4d5f97233f654b
Benjamin Heasly <benjamin.heasly@gmail.com>
2023-08-25 17:24:29 -0400

    Initial Open Ephys ZMQ message formatting/parsing for Pyramid.

--- pyramid/src/pyramid/neutral_zone/readers/open_ephys_zmq.py
+++ pyramid/src/pyramid/neutral_zone/readers/open_ephys_zmq.py
@@ -1,10 +1,140 @@
 from types import TracebackType
-from typing import Any, ContextManager, Self
+from typing import ContextManager, Self
 import uuid
 import json
 
+import numpy as np
 import zmq
 
+
+# TODO: revisit after reviewing source code (seems nice and clear) rather than docs (seems incomplete):
+# https://open-ephys.github.io/gui-docs/User-Manual/Plugins/ZMQ-Interface.html
+# https://github.com/open-ephys-plugins/zmq-interface/blob/main/Source/ZmqInterface.cpp#L359
+
+
+def format_heartbeat(
+    uuid: str,
+    application: str = "Pyramid",
+    encoding: str = 'utf-8'
+) -> bytes:
+    heartbeat_info = {
+        "application": application,
+        "uuid": uuid,
+        "type": "heartbeat"
+    }
+    heartbeat_bytes = json.dumps(heartbeat_info).encode(encoding=encoding)
+    return heartbeat_bytes
+
+
+def parse_heartbeat(
+    message: bytes,
+    encoding: str = 'utf-8'
+) -> dict[str, str]:
+    return json.loads(message.decode(encoding=encoding))
+
+
+def format_continuous_data(
+        data: np.ndarray,
+        stream_name: str,
+        channel_num: int,
+        sample_num: int,
+        sample_rate: float,
+        encoding: str = 'utf-8'
+) -> list[bytes]:
+    header_info = {
+        "stream": stream_name,
+        "channel_num": channel_num,
+        "num_samples": data.size,
+        "sample_num": sample_num,
+        "sample_rate": sample_rate
+    }
+    header_bytes = json.dumps(header_info).encode(encoding=encoding)
+    return [header_bytes, data.tobytes()]
+
+
+def parse_continuous_data(
+    parts: list[bytes],
+    encoding: str = 'utf-8',
+    dtype = np.float32
+) -> tuple[dict, np.ndarray]:
+    header_info = json.loads(parts[0].decode(encoding=encoding))
+    data = np.frombuffer(parts[1], dtype=dtype)
+    return (header_info, data)
+
+
+def format_event(
+    stream_name: str,
+    source_node: int,
+    type: int,
+    sample_num: int,
+    event_line: int,
+    event_state: int,
+    ttl_word: int,
+    encoding: str = 'utf-8'
+) -> list[bytes]:
+    header_info = {
+        "stream": stream_name,
+        "source_node": source_node,
+        "type": type,
+        "sample_num": sample_num
+    }
+    header_bytes = json.dumps(header_info).encode(encoding=encoding)
+    data = bytes([event_line, event_state]) + ttl_word.to_bytes(length=8)
+    return [header_bytes, data]
+
+
+def parse_event(
+    parts: list[bytes],
+    encoding: str = 'utf-8'
+) -> tuple[dict, int, int, int]:
+    header_info = json.loads(parts[0].decode(encoding=encoding))
+    data = parts[1]
+    event_line = int(data[0])
+    event_state = int(data[1])
+    ttl_word = int.from_bytes(data[2:10])
+    return (header_info, event_line, event_state, ttl_word)
+
+
+def format_spike(
+    waveform: np.ndarray,
+    stream_name: str,
+    source_node: int,
+    electrode: str,
+    sample_num: int,
+    sorted_id: int,
+    threshold: list[float],
+    encoding: str = 'utf-8'
+) -> list[bytes]:
+    if len(waveform.shape) == 2:
+        num_channels = waveform.shape[0]
+        num_samples = waveform.shape[1]
+    else:
+        num_channels = 1
+        num_samples = waveform.size
+    header_info = {
+        "stream": stream_name,
+        "source_node": source_node,
+        "electrode": electrode,
+        "sample_num": sample_num,
+        "num_channels": num_channels,
+        "num_samples": num_samples,
+        "sorted_id": sorted_id,
+        "threshold": threshold
+    }
+    header_bytes = json.dumps(header_info).encode(encoding=encoding)
+    return [header_bytes, waveform.tobytes()]
+
+
+def parse_spike(
+    parts: list[bytes],
+    encoding: str = 'utf-8',
+    dtype = np.float32
+) -> tuple[dict, np.ndarray]:
+    header_info = json.loads(parts[0].decode(encoding=encoding))
+    num_channels = header_info.get("num_channels", 1)
+    num_samples = header_info.get("num_samples", -1)
+    waveform = np.frombuffer(parts[1], dtype=dtype).reshape([num_channels, num_samples])
+    return (header_info, waveform)
 
 
 class Client(ContextManager):
@@ -19,17 +149,11 @@ class Client(ContextManager):
     ) -> None:
         self.address = f"{scheme}://{host}:{port}"
         self.encoding = encoding
+        self.heartbeat_message = format_heartbeat(uuid)
 
         self.context = None
         self.poller = None
         self.socket = None
-
-        heartbeat_info = {
-            "application" : "Pyramid",
-            "uuid" : uuid,
-            "type": "heartbeat"
-        }
-        self.heartbeat_message = json.dumps(heartbeat_info)
 
     def __enter__(self) -> Self:
         self.context = zmq.Context()
@@ -111,4 +235,3 @@ class Server(ContextManager):
     def send_reply(self, messages: list[str]) -> None:
         parts = [message.encode(self.encoding) for message in messages]
         self.socket.send_multipart(parts)
-
--- pyramid/tests/pyramid/neutral_zone/readers/test_open_ephys_zmq.py
+++ pyramid/tests/pyramid/neutral_zone/readers/test_open_ephys_zmq.py
@@ -1,5 +1,19 @@
-import zmq
-from pyramid.neutral_zone.readers.open_ephys_zmq import Client, Server
+import uuid
+
+import numpy as np
+
+from pyramid.neutral_zone.readers.open_ephys_zmq import (
+    Client,
+    Server,
+    format_heartbeat,
+    parse_heartbeat,
+    format_continuous_data,
+    parse_continuous_data,
+    format_event,
+    parse_event,
+    format_spike,
+    parse_spike
+)
 
 
 def test_zmq_basics():
@@ -25,3 +39,93 @@ def test_zmq_basics():
 
     assert server.context is None
     assert client.context is None
+
+
+def test_heartbeat_format():
+    application = "Test"
+    id = str(uuid.uuid4())
+    message = format_heartbeat(uuid=id, application=application)
+    heartbeat = parse_heartbeat(message)
+    assert heartbeat["application"] == application
+    assert heartbeat["uuid"] == id
+    assert heartbeat["type"] == "heartbeat"
+
+
+def test_continuous_data_format():
+    data = np.arange(1000, dtype=np.float32)
+    stream_name = "Test"
+    channel_num = 41
+    sample_num = 42
+    sample_rate = 1000
+    parts = format_continuous_data(data, stream_name, channel_num, sample_num, sample_rate)
+    (header, data_2) = parse_continuous_data(parts)
+    assert header["stream"] == stream_name
+    assert header["channel_num"] == channel_num
+    assert header["sample_num"] == sample_num
+    assert header["sample_rate"] == sample_rate
+    assert header["num_samples"] == data.size
+    assert np.array_equal(data_2, data)
+
+
+def test_event_format():
+    stream_name = "Test"
+    source_node = 42
+    type = 3
+    sample_num = 43
+    event_line = 7
+    event_state = 1
+    ttl_word = 65535
+    parts = format_event(stream_name, source_node, type, sample_num, event_line, event_state, ttl_word)
+    (header, event_line_2, event_state_2, ttl_word_2) = parse_event(parts)
+    assert header["stream"] == stream_name
+    assert header["source_node"] == source_node
+    assert header["type"] == type
+    assert header["sample_num"] == sample_num
+    assert event_line_2 == event_line
+    assert event_state_2 == event_state
+    assert ttl_word_2 == ttl_word
+
+
+def test_spike_format_single_channel():
+    num_samples = 1000
+    waveform = np.arange(num_samples, dtype=np.float32)
+    stream_name = "Test"
+    source_node = 42
+    electrode = "Testrode"
+    sample_num = 123
+    sorted_id = 7
+    threshold = [20, 21]
+    parts = format_spike(waveform, stream_name, source_node, electrode, sample_num, sorted_id, threshold)
+    (header, waveform_2) = parse_spike(parts)
+    assert header["stream"] == stream_name
+    assert header["source_node"] == source_node
+    assert header["electrode"] == electrode
+    assert header["sample_num"] == sample_num
+    assert header["num_channels"] == 1
+    assert header["num_samples"] == num_samples
+    assert header["sorted_id"] == sorted_id
+    assert header["threshold"] == threshold
+    assert np.array_equal(waveform_2, waveform.reshape([1, num_samples]))
+
+
+def test_spike_format_multiple_channels():
+    num_channels = 10
+    num_samples = 100
+    waveform = np.arange(num_channels * num_samples, dtype=np.float32).reshape([num_channels, num_samples])
+    stream_name = "Test"
+    source_node = 42
+    electrode = "Testrode"
+    sample_num = 123
+    sorted_id = 7
+    threshold = [20, 21]
+    parts = format_spike(waveform, stream_name, source_node, electrode, sample_num, sorted_id, threshold)
+    (header, waveform_2) = parse_spike(parts)
+    assert header["stream"] == stream_name
+    assert header["source_node"] == source_node
+    assert header["electrode"] == electrode
+    assert header["sample_num"] == sample_num
+    assert header["num_channels"] == num_channels
+    assert header["num_samples"] == num_samples
+    assert header["sorted_id"] == sorted_id
+    assert header["threshold"] == threshold
+    assert np.array_equal(waveform_2, waveform)
